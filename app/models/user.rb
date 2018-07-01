acd87d842e0aaa4ff6d71ed58a28f206e45a74ee
TPei <t.s.peikert@gmail.com>
2018-07-01 23:28:16 +0200

    add users model and controller skeleton

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  before_save { email.downcase! }
+
+  validates :name, presence: true
+
+  validates :email,
+    presence: true,
+    uniqueness: { case_sensitive: false },
+    format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  validates :password,
+    presence: true,
+    length: { minimum: 8 }
+
+  has_secure_password
+end
