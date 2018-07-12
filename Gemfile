fde8ba744e0237a580732eed75a5321811b7ff11
paul <paul.wille@t-online.de>
2018-07-12 12:03:13 +0200

    add api calls for steps and heart_rate; prepared purposefull queries; optional params in as_json for user; api scope in routes

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
 
-gem 'rails', path: "/home/tpei/Code/rails"
+gem 'rails', path: "/Users/Oleg1/Documents/Wille/Studium/TU/Semester\ 4/por/rails"
 
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
@@ -43,6 +43,8 @@ gem 'jquery-rails'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'better_errors'
+  gem 'binding_of_caller'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -14,11 +14,19 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  get    '/api/user/:id/purpose/:code',
-    to: 'api#retrieve_user'
+  scope "/api" do
+    get    'users/:id/purpose/:code',
+      to: 'api#retrieve_user'
 
-  get    '/api/users/purpose/:code',
-    to: 'api#retrieve_all_users'
+    get    'users/:id/steps/purpose/:code',
+      to: 'api#get_steps'
+
+    get    'users/:id/heart_rate_logs/purpose/:code',
+      to: 'api#get_heart_rate_logs'
+
+    get    'users/purpose/:code',
+      to: 'api#retrieve_all_users'
+  end
 
   root 'sessions#new'
 end
