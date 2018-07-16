08d86d4f28cc275915529a74182e764ad1654221
Karl Wolf <wolfkarl@gmail.com>
2018-07-16 09:46:26 +0200

    remove all for queries

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,22 +1,22 @@
 class ApiController < ApplicationController
 
   def retrieve_user
-    user = User.for(purpose).find(id)
+    user = User.find(id)
     render json: user.as_json(include_steps: false, include_heart_rate: false)
   end
 
   def retrieve_all_users
-    users = User.all#.for(purpose)
+    users = User.all
     render json: users.map(&:as_json)
   end
 
   def get_steps
-    steps = User.for(purpose).find(id).heart_rate_logs.for(purpose)
+    steps = User.find(id).heart_rate_logs
     render json: steps.map(&:as_json)
   end
 
   def get_heart_rate_logs
-    logs = User.for(purpose).find(id).heart_rate_logs.for(purpose)
+    logs = User.find(id).heart_rate_logs
     render json: logs.map(&:as_json)
   end
 
